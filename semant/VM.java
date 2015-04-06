16ba5641ba19ec4fa807612e39bcdc2798cc7161
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-04-06 18:38:09 +0200

    Add switch cases for VM

--- semant/VM.java
+++ semant/VM.java
@@ -20,6 +20,42 @@ public class VM {
         // Break execution when no more code is available.
         if (stepCounter == code.size()) return false;
 
+        switch (code.get(stepCounter).opcode) {
+            case ADD:
+                break;
+            case AND:
+                break;
+            case BRANCH:
+                break;
+            case EQ:
+                break;
+            case FALSE:
+                break;
+            case FETCH:
+                break;
+            case LE:
+                break;
+            case LOOP:
+                break;
+            case MULT:
+                break;
+            case NEG:
+                break;
+            case NOOP:
+                break;
+            case PUSH:
+                break;
+            case STORE:
+                break;
+            case SUB:
+                break;
+            case TRUE:
+                break;
+            default:
+                System.err.println("Invalid opcode");
+                System.exit(1);
+        }
+
         ++stepCounter;
         return true;
     }
