715a91604e4f95168654bf840315df0cc2ad45f4
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-04-06 20:44:54 +0200

    Work on switch for opcodes

--- semant/Configuration.java
+++ semant/Configuration.java
@@ -1,9 +1,11 @@
 package semant;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 public class Configuration {
     private HashMap<String, Integer> symTable;
+    private Stack<Integer> stack;
 
     public Configuration() {
         symTable = new HashMap<String, Integer>();
@@ -21,5 +23,19 @@ public class Configuration {
      */
     public int getVar(String var) {
         return symTable.get(var);
+    }
+
+    /**
+     * Push the given value on the stack.
+     */
+    public void pushStack(int val) {
+        stack.push(val);
+    }
+
+    /**
+     * Pop one value from the stack.
+     */
+    public int popStack() {
+        return stack.pop();
     }
 }
--- semant/VM.java
+++ semant/VM.java
@@ -1,13 +1,15 @@
 package semant;
 
-import semant.amsyntax.Code;
+import semant.amsyntax.*;
 
 public class VM {
     private Code code;
+    private Configuration conf;
     private int stepCounter = 0;
 
     public VM(Code code) {
         this.code = code;
+        conf = new Configuration(); // TODO Should not be global
     }
 
     // public Configuration step(Configuration conf)
@@ -20,36 +22,70 @@ public class VM {
         // Break execution when no more code is available.
         if (stepCounter == code.size()) return false;
 
-        switch (code.get(stepCounter).opcode) {
+        Inst inst = code.get(stepCounter);
+        int a, a1, a2;
+        switch (inst.opcode) {
             case ADD:
+                a1 = conf.popStack();
+                a2 = conf.popStack();
+                conf.pushStack(a1 + a2);
                 break;
             case AND:
+                a1 = conf.popStack();
+                a2 = conf.popStack();
+                conf.pushStack((a1 == 1) && (a2 == 1) ? 1 : 0);
                 break;
             case BRANCH:
+                a = conf.popStack();
+                // TODO Correct?
+                if (a == 1) code = ((Branch) inst).c1;
+                else code = ((Branch) inst).c2;
                 break;
             case EQ:
+                a1 = conf.popStack();
+                a2 = conf.popStack();
+                conf.pushStack(a1 == a2 ? 1 : 0);
                 break;
             case FALSE:
+                conf.pushStack(0); // False
                 break;
             case FETCH:
+                a = conf.getVar(((Fetch) inst).x);
+                conf.pushStack(a);
                 break;
             case LE:
+                a1 = conf.popStack();
+                a2 = conf.popStack();
+                conf.pushStack(a1 <= a2 ? 1 : 0);
                 break;
             case LOOP:
+                // TODO
                 break;
             case MULT:
+                a1 = conf.popStack();
+                a2 = conf.popStack();
+                conf.pushStack(a1 * a2);
                 break;
             case NEG:
+                a = conf.popStack();
+                conf.pushStack((a == 1) ? 0 : 1);
                 break;
             case NOOP:
                 break;
             case PUSH:
+                conf.pushStack(((Push) inst).getValue());
                 break;
             case STORE:
+                a = conf.popStack();
+                conf.setVar(((Store) inst).x, a);
                 break;
             case SUB:
+                a1 = conf.popStack();
+                a2 = conf.popStack();
+                conf.pushStack(a1 - a2);
                 break;
             case TRUE:
+                conf.pushStack(1); // True
                 break;
             default:
                 System.err.println("Invalid opcode");
